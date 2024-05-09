858c57bdeb72865b684337b6399917f4bb4a42fd
Luis <luisarquescalero@gmail.com>
2024-05-09 18:39:59 +0200

    README update and first info

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,34 +1,58 @@
-const reactDescriptions= ['Fundamental', 'Crucial', 'Core'];
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
+	return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  
-  return (
-    <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  )
+	const description = reactDescriptions[genRandomInt(2)];
+
+	return (
+		<header>
+			<img src={reactImg} alt='Stylized atom' />
+			<h1>React Essentials</h1>
+			<p>
+				{description} React concepts you will need for almost any app you are
+				going to build!
+			</p>
+		</header>
+	);
 }
 
+function CoreConcept(props) {
+	return (
+		<li>
+			<img src={props.image} alt={props.title} />
+			<h3>{props.title}</h3>
+			<p>{props.description}</p>
+		</li>
+	);
+}
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<main>
+				<section id='core-concepts'>
+					<h2>Core Concept</h2>
+					<ul>
+						<CoreConcept
+							title='Components'
+							description='The core UI building block.'
+              image={componentsImg}
+						/>
+						<CoreConcept />
+						<CoreConcept />
+						<CoreConcept />
+					</ul>
+				</section>
+			</main>
+		</div>
+	);
 }
 
 export default App;
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# React-Projects
+Proyectos de React - Curso Completo de Udemy
+
+Este repositorio contiene los proyectos que estoy desarrollando como parte del curso "La Guía Completa de React 2024" en Udemy.
+
+Objetivo
+El objetivo de este repositorio es documentar mi progreso en el aprendizaje de React y compartir los proyectos que desarrollaré a lo largo del curso.
+
+Contenido
+En este repositorio encontrarás una carpeta para cada proyecto que realice durante el curso. Cada carpeta contendrá el código fuente del proyecto y cualquier archivo adicional necesario para ejecutarlo.
+
+Cursos
+La Guía Completa de React
+
+Autor
+Luis Arques Calero
+
+Agradecimientos
+Agradecimientos al equipo de Udemy por proporcionar un curso tan completo y profundo sobre React.
+
+¡Gracias por visitar este repositorio y seguir mi progreso en el aprendizaje de React! Si tienes alguna sugerencia o comentario, ¡no dudes en hacérmelo saber!
