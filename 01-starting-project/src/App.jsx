ee541c374aea51676bd9f5188f9dac1be5f7b52f
Luis <luisarquescalero@gmail.com>
2024-05-15 17:25:14 +0200

    Components folders organization refactor

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,36 +1,7 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-	return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-	const description = reactDescriptions[genRandomInt(2)];
-
-	return (
-		<header>
-			<img src={reactImg} alt='Stylized atom' />
-			<h1>React Essentials</h1>
-			<p>
-				{description} React concepts you will need for almost any app you are
-				going to build!
-			</p>
-		</header>
-	);
-}
-
-function CoreConcept({ image, title, description }) {
-	return (
-		<li>
-			<img src={image} alt={title} />
-			<h3>{title}</h3>
-			<p>{description}</p>
-		</li>
-	);
-}
 
 function App() {
 	return (
