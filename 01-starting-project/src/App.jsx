09e8a69e8c66ab4597127c4027ad8f04ae62d47a
Luis <luisarquescalero@gmail.com>
2024-05-20 18:46:53 +0200

    Dinamyc content and styling

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,12 +2,16 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
-
+import { useState } from 'react';
+import { EXAMPLES } from './data-with-examples.js';
 
 function App() {
+	const [selectedTopic, setSelectedTopic] = useState();
+
 	const handleSelect = (selectedButton) => {
-        console.log(selectedButton);
-    };
+		setSelectedTopic(selectedButton);
+		console.log(selectedTopic);
+	};
 
 	return (
 		<div>
@@ -16,26 +20,59 @@ function App() {
 				<section id='core-concepts'>
 					<h2>Core Concept</h2>
 					<ul>
-						<CoreConcept {...CORE_CONCEPTS[0]}/>
-						<CoreConcept {...CORE_CONCEPTS[1]}/>
-						<CoreConcept {...CORE_CONCEPTS[2]}/> {/*Esta es la forma corta con el spread operator. Hace exactamente lo mismo que la linea de abajo pero con mucho menos codigo. */}
-
+						<CoreConcept {...CORE_CONCEPTS[0]} />
+						<CoreConcept {...CORE_CONCEPTS[1]} />
+						<CoreConcept {...CORE_CONCEPTS[2]} />{' '}
+						{/*Esta es la forma corta con el spread operator. Hace exactamente lo mismo que la linea de abajo pero con mucho menos codigo. */}
 						<CoreConcept
-						title={CORE_CONCEPTS[3].title}
-						description={CORE_CONCEPTS[3].description}
-              			image={CORE_CONCEPTS[3].image}
+							title={CORE_CONCEPTS[3].title}
+							description={CORE_CONCEPTS[3].description}
+							image={CORE_CONCEPTS[3].image}
 						/>
 					</ul>
 				</section>
-				<section id="examples">
+				<section id='examples'>
 					<h2>Examples</h2>
 					<menu>
-						<TabButton onSelect={() => handleSelect('Components')} children="Components"/> {/*Esta es la forma larga de hacerlo. */}
-						<TabButton onSelect={() => handleSelect('JSX')}> JSX </TabButton>
-						<TabButton onSelect={() => handleSelect('Props')}> Props </TabButton> 
-						<TabButton onSelect={() => handleSelect('State')}>State</TabButton>
+						<TabButton
+							isSelected={selectedTopic === 'components'}
+							onSelect={() => handleSelect('components')}
+							children='Components'
+						/>{' '}
+						{/*Esta es la forma larga de hacerlo. */}
+						<TabButton
+							isSelected={selectedTopic === 'jsx'}
+							onSelect={() => handleSelect('jsx')}
+						>
+							{' '}
+							JSX{' '}
+						</TabButton>
+						<TabButton
+							isSelected={selectedTopic === 'props'}
+							onSelect={() => handleSelect('props')}
+						>
+							{' '}
+							Props{' '}
+						</TabButton>
+						<TabButton
+							isSelected={selectedTopic === 'state'}
+							onSelect={() => handleSelect('state')}
+						>
+							State
+						</TabButton>
 					</menu>
 
+					{!selectedTopic ? (
+						<p>Please select a topic.</p>
+					) : (
+						<div id='tab-content'>
+							<h3>{EXAMPLES[selectedTopic].title}</h3>
+							<p>{EXAMPLES[selectedTopic].description}</p>
+							<pre>
+								<code>{EXAMPLES[selectedTopic].code}</code>
+							</pre>
+						</div>
+					)}
 				</section>
 			</main>
 		</div>
