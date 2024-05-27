391c90491b96a475cffa6bd0f7f2eee4dd370845
Luis <luisarquescalero@gmail.com>
2024-05-27 23:50:21 +0200

    Add header and update App component to include Player components

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,10 +1,16 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+	return (
+		<main>
+			<div id='game-container'>
+				<ol id='players'>
+					<Player name="Player 1" symbol="X"/>
+          <Player name="Player 2" symbol="O"/>
+				</ol>
+			</div>
+		</main>
+	);
 }
 
-export default App
+export default App;
