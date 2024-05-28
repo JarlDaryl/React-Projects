2d6c3a416b8c2a0ae770641f933ec47a6e0cda1e
Luis <luisarquescalero@gmail.com>
2024-05-28 22:55:46 +0200

    Refactor Player component to allow editing player name

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,12 +1,12 @@
-import Player from "./components/Player";
+import Player from './components/Player';
 
 function App() {
 	return (
 		<main>
 			<div id='game-container'>
 				<ol id='players'>
-					<Player name="Player 1" symbol="X"/>
-          <Player name="Player 2" symbol="O"/>
+					<Player name='Player 1' symbol='X' />
+					<Player name='Player 2' symbol='O' />
 				</ol>
 			</div>
 		</main>
--- 02-tic-tac-toe/src/components/Player.jsx
+++ 02-tic-tac-toe/src/components/Player.jsx
@@ -1,10 +1,28 @@
-export default function Player({name, symbol}) {
+import { useState } from 'react';
+
+export default function Player({ name, symbol }) {
+	const [isEditing, setIsEditing] = useState(false);
+
+	const handleEdit = () => {
+		setIsEditing((editing) => !editing);
+	};
+
+	let playerName = <span className='player-name'>{name}</span>
+	//let buttonCapttion = 'Edit';
+
+	if ( isEditing ) {
+		playerName = <input type='text' required value={name}/>
+		//buttonCapttion = 'Save';
+	}
+
 	return (
 		<li>
 			<span className='player'>
-				<span className='player-name'>{name}</span>
-				<span className='player-symbol'>{symbol}</span>
-				<button>Edit</button>
+					<div>
+						{playerName}
+						<span className='player-symbol'>{symbol}</span>
+						<button onClick={handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
+					</div>
 			</span>
 		</li>
 	);
