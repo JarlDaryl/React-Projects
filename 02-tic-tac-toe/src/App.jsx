317eaf601785c49a580390fabbe6d9698850895a
Luis <luisarquescalero@gmail.com>
2024-06-05 18:07:57 +0200

    Project done with all functions working

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player';
 import GameBoard from './components/GameBoard';
 import Log from './components/Log';
+import GameOver from './components/GameOver.jsx';
 import { useState } from 'react';
-import { WINNING_COMBINATIONS } from './winning-combinations';
+import {WINNING_COMBINATIONS} from './winning-combinations.js';
 
-const initialGameBoard = [
+const PLAYERS = {
+	X: 'Player 1',
+	O: 'Player 2',
+}
+
+const INITIAL_GAME_BOARD = [
 	[null, null, null],
 	[null, null, null],
 	[null, null, null],
@@ -18,12 +24,8 @@ function deriveActivePlayer(gameTurns) {
 	return currentPlayer;
 }
 
-function App() {
-	const [gameTurns, setGameTurns] = useState([]);
-
-	const activePlayer = deriveActivePlayer(gameTurns);
-
-	let gameBoard = initialGameBoard;
+function deriveGameBoard(gameTurns) {
+	let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
 
 	for (const turn of gameTurns) {
 		const { square, player } = turn;
@@ -31,23 +33,36 @@ function App() {
 
 		gameBoard[row][col] = player;
 	}
+	return gameBoard;
+}
 
+function deriveWinner(gameBoard, players) {
 	let winner;
 
 	for (const combination of WINNING_COMBINATIONS) {
-		const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
-		const secondSquareSymbol = gameBoard[combination[1].row][combination[1].col];
-		const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
+		const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+		const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+		const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
 
 		if (
 			firstSquareSymbol &&
 			firstSquareSymbol === secondSquareSymbol &&
 			firstSquareSymbol === thirdSquareSymbol
 		) {
-			winner = firstSquareSymbol;
-			console.log(winner)
+			winner = players[firstSquareSymbol];
 		}
 	}
+	return winner;
+}
+
+function App() {
+	const [players, setPlayers] = useState(PLAYERS);
+	const [gameTurns, setGameTurns] = useState([]);
+
+	const activePlayer = deriveActivePlayer(gameTurns);
+	const gameBoard = deriveGameBoard(gameTurns);
+	const winner = deriveWinner(gameBoard, players);
+	const hasDraw = gameTurns.length === 9 && !winner;
 
 	function handleSelectSquare(rowIndex, colIndex) {
 		setGameTurns((prevTurns) => {
@@ -61,22 +76,37 @@ function App() {
 		});
 	}
 
+	function handleRematch() {
+		setGameTurns([]);
+	}
+
+	const handlePlayerChangeName = (symbol, newName) => {
+		setPlayers(prevPlayers => {
+			return {
+				...prevPlayers,
+				[symbol]: newName,
+			}
+		})
+	}
+
 	return (
 		<main>
 			<div id='game-container'>
 				<ol id='players' className='highlight-player'>
 					<Player
-						initialName='Player 1'
+						initialName={PLAYERS.X}
 						symbol='X'
 						isActive={activePlayer === 'X'}
+						onChangeName={handlePlayerChangeName}
 					/>
 					<Player
-						initialName='Player 2'
+						initialName={PLAYERS.O}
 						symbol='O'
 						isActive={activePlayer === 'O'}
+						onChangeName={handlePlayerChangeName}
 					/>
 				</ol>
-				{winner && <p>You won, {winner}!</p>}
+				{(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRematch}/>}
 				<GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
 			</div>
 			<Log turns={gameTurns} />
--- 02-tic-tac-toe/src/components/Player.jsx
+++ 02-tic-tac-toe/src/components/Player.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
 	const [playerName, setPlayerName] = useState(initialName);
 	const [isEditing, setIsEditing] = useState(false);
 
 	const handleEdit = () => {
 		setIsEditing((editing) => !editing);
+		
+		if(isEditing) {
+			onChangeName(symbol, playerName)
+		}
 	};
 
 	function handleChange(event) {
