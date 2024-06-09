f08f6db8b717b5e5a82b6e6c2c340f386ae42711
Luis <luisarquescalero@gmail.com>
2024-06-09 17:33:38 +0200

    Tailwind Implementation

--- 04-styling-components/src/components/AuthInputs.jsx
+++ 04-styling-components/src/components/AuthInputs.jsx
@@ -1,53 +1,56 @@
 import { useState } from 'react';
 
+import Button from './Button';
+import Input from './Input';
+
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [submitted, setSubmitted] = useState(false);
+	const [enteredEmail, setEnteredEmail] = useState('');
+	const [enteredPassword, setEnteredPassword] = useState('');
+	const [submitted, setSubmitted] = useState(false);
 
-  function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
-      setEnteredEmail(value);
-    } else {
-      setEnteredPassword(value);
-    }
-  }
+	function handleInputChange(identifier, value) {
+		if (identifier === 'email') {
+			setEnteredEmail(value);
+		} else {
+			setEnteredPassword(value);
+		}
+	}
 
-  function handleLogin() {
-    setSubmitted(true);
-  }
+	function handleLogin() {
+		setSubmitted(true);
+	}
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
-  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+	const emailNotValid = submitted && !enteredEmail.includes('@');
+	const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
-  return (
-    <div id="auth-inputs">
-      <div className="controls">
-        <p>
-          <label>Email</label>
-          <input
-            type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <label>Password</label>
-          <input
-            type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
-      </div>
-      <div className="actions">
-        <button type="button" className="text-button">
-          Create a new account
-        </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			id='auth-inputs'
+			className='w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'
+		>
+			<div className='flex flex-col gap-2 mb-6'>
+				<Input
+					label='Email'
+					type='email'
+					invalid={emailNotValid}
+					onChange={(event) => handleInputChange('email', event.target.value)}
+				/>
+
+				<Input
+					label='Password'
+					type='password'
+					invalid={passwordNotValid}
+					onChange={(event) => handleInputChange('email', event.target.value)}
+				/>
+			</div>
+			<div className='flex justify-end gap-4'>
+				<button type='button' className='text-amber-400 hover:text-amber-500'>
+					Create a new account
+				</button>
+				<Button className='Button' onClick={handleLogin}>
+					Sign In
+				</Button>
+			</div>
+		</div>
+	);
 }
--- 04-styling-components/src/components/Header.jsx
+++ 04-styling-components/src/components/Header.jsx
@@ -1,12 +1,18 @@
 import logo from '../assets/logo.png';
-import  './Header.css';
+
 
 export default function Header() {
-  return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
-    </header>
-  );
+	return (
+		<header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
+			<img
+				src={logo}
+				alt='A canvas'
+				className='object-contain mb-8 w-44 h-44'
+			/>
+			<h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-tit">
+				ReactArt
+			</h1>
+			<p className="text-stone-500">A community of artists and art-lovers.</p>
+		</header>
+	);
 }
