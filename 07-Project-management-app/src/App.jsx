ecd7cd349515b0e2d882c8c3ba2656ef31e39e64
Luis <luisarquescalero@gmail.com>
2024-06-16 13:43:49 +0200

    Project set up and firsts functionalities

---
+++ 07-Project-management-app/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
+import ProjectsSideBar from './components/ProjectsSideBar';
+
+function App() {
+	const [projectsState, setProjectsState] = useState({
+		selectedProjectId: undefined,
+		projects: [],
+	});
+
+	function handleStartAddProject() {
+		setProjectsState((prevState) => {
+			return {
+				...prevState,
+				selectedProjectId: null,
+			};
+		});
+	}
+
+	function handleAddProject(projectData) {
+		setProjectsState((prevState) => {
+			const projectId = Math.random();
+			const newProject = {
+				...projectData,
+				id: projectId,
+			};
+			return {
+				...prevState,
+				selectedProjectId: undefined,
+				projects: [...prevState.projects, newProject],
+			};
+		});
+	}
+
+	let content;
+
+	if (projectsState.selectedProjectId === null) {
+		content = <NewProject onAdd={handleAddProject} />;
+	} else if (projectsState.selectedProjectId === undefined) {
+		content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+	}
+
+	return (
+		<main className='h-screen my-8 flex gap-8'>
+			<ProjectsSideBar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+			{content}
+		</main>
+	);
+}
+
+export default App;
