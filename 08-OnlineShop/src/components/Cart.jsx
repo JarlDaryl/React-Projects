3cfc68471ca3e633f9da0a72a9f413999a013a44
Luis <luisarquescalero@gmail.com>
2024-06-19 23:55:18 +0200

    chore: Refactor CartModal and Header components, update Cart component

--- 08-OnlineShop/src/components/Cart.jsx
+++ 08-OnlineShop/src/components/Cart.jsx
@@ -2,8 +2,10 @@ import { useContext } from "react";
 import { CartContext } from "../store/shoppingCartContext";
 
 
-export default function Cart({ onUpdateItemQuantity }) {
+export default function Cart() {
   const cartCtx = useContext(CartContext);
+  const {items, updateItemQuantity} = useContext(CartContext);
+
 
   const totalPrice = cartCtx.items.reduce(
     (acc, item) => acc + item.price * item.quantity,
@@ -26,11 +28,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
