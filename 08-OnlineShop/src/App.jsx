ca176de457b0855e97989163c283fc0ccc261ebd
Luis <luisarquescalero@gmail.com>
2024-06-19 18:20:49 +0200

    Implementing UseContext

--- 08-OnlineShop/src/App.jsx
+++ 08-OnlineShop/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react';
 
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
+import Product from './components/Product.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
+import { CartContext } from './store/shoppingCartContext.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -64,15 +66,25 @@ function App() {
       };
     });
   }
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  }
 
   return (
-    <>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
-    </>
+      <Shop >
+      {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} onAddToCart={handleAddItemToCart} />
+          </li>
+        ))}
+      </Shop>
+    </CartContext.Provider>
   );
 }
 
--- 08-OnlineShop/src/components/Product.jsx
+++ 08-OnlineShop/src/components/Product.jsx
@@ -1,11 +1,15 @@
+import { CartContext } from "../store/shoppingCartContext";
+import { useContext } from "react";
+
 export default function Product({
   id,
   image,
   title,
   price,
   description,
-  onAddToCart,
 }) {
+
+  const {addItemToCart} = useContext(CartContext);
   return (
     <article className="product">
       <img src={image} alt={title} />
@@ -16,7 +20,7 @@ export default function Product({
           <p>{description}</p>
         </div>
         <p className='product-actions'>
-          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
+          <button onClick={() => addItemToCart(id)}>Add to Cart</button>
         </p>
       </div>
     </article>
