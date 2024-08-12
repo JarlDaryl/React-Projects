82ffa419400794b704969e7f129f46f2aa250582
Luis <luisarquescalero@gmail.com>
2024-08-12 20:12:09 +0200

    Update shopping cart context to use reducer instead of useState

--- 08-OnlineShop/src/store/shoppingCartContext.jsx
+++ 08-OnlineShop/src/store/shoppingCartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
 import { DUMMY_PRODUCTS } from '../dummy-products';
 
 export const CartContext = createContext({
@@ -7,17 +7,12 @@ export const CartContext = createContext({
 	updateItemQuantity: () => {},
 });
 
-export default function CartContextProvider({ children }) {
-	const [shoppingCart, setShoppingCart] = useState({
-		items: [],
-	});
-
-	function handleAddItemToCart(id) {
-		setShoppingCart((prevShoppingCart) => {
-			const updatedItems = [...prevShoppingCart.items];
+function shoppingCartReducer(state, action) {
+	if(action.type === 'ADD_ITEM') {
+		const updatedItems = [...state.items];
 
 			const existingCartItemIndex = updatedItems.findIndex(
-				(cartItem) => cartItem.id === id
+				(cartItem) => cartItem.id === action.payload
 			);
 			const existingCartItem = updatedItems[existingCartItemIndex];
 
@@ -28,9 +23,9 @@ export default function CartContextProvider({ children }) {
 				};
 				updatedItems[existingCartItemIndex] = updatedItem;
 			} else {
-				const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+				const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload);
 				updatedItems.push({
-					id: id,
+					id: action.payload,
 					name: product.title,
 					price: product.price,
 					quantity: 1,
@@ -40,21 +35,19 @@ export default function CartContextProvider({ children }) {
 			return {
 				items: updatedItems,
 			};
-		});
 	}
 
-	function handleUpdateCartItemQuantity(productId, amount) {
-		setShoppingCart((prevShoppingCart) => {
-			const updatedItems = [...prevShoppingCart.items];
+	if (action.type === 'UPDATE_ITEM') {
+		const updatedItems = [...state.items];
 			const updatedItemIndex = updatedItems.findIndex(
-				(item) => item.id === productId
+				(item) => item.id === action.payload.productId
 			);
 
 			const updatedItem = {
 				...updatedItems[updatedItemIndex],
 			};
 
-			updatedItem.quantity += amount;
+			updatedItem.quantity += action.payload.amount;
 
 			if (updatedItem.quantity <= 0) {
 				updatedItems.splice(updatedItemIndex, 1);
@@ -65,17 +58,41 @@ export default function CartContextProvider({ children }) {
 			return {
 				items: updatedItems,
 			};
-		});
+	}
+
+	return state;
+}
+
+export default function CartContextProvider({ children }) {
+	const [shoppingCartState, shoppingCartDispatch] = useReducer(
+		shoppingCartReducer,
+		{
+			items: [],
+		}
+	);
+
+
+	function handleAddItemToCart(id) {
+		shoppingCartDispatch({ type: 'ADD_ITEM', payload: id });
+
+	}
+
+	function handleUpdateCartItemQuantity(productId, amount) {
+		shoppingCartDispatch({
+			type: 'UPDATE_ITEM',
+			payload: {
+				productId,
+				amount,
+			}
+		})
 	}
 	const ctxValue = {
-		items: shoppingCart.items,
+		items: shoppingCartState.items,
 		addItemToCart: handleAddItemToCart,
 		updateItemQuantity: handleUpdateCartItemQuantity,
 	};
 
 	return (
-		<CartContext.Provider value={ctxValue}>
-            {children}
-        </CartContext.Provider>
+		<CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
 	);
 }
---
+++ 09-PlacePicker/src/components/DeleteConfirmation.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+const TIMER = 3000;
+
+export default function DeleteConfirmation({ onConfirm, onCancel }) {
+	const [remainingTime, setRemainingTime] = useState(TIMER);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setRemainingTime((prevTime) => prevTime - 10);
+		}, 10);
+
+    return () => {
+      clearInterval(interval);
+    }
+	}, []);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			onConfirm();
+		}, TIMER);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [onConfirm]);
+
+	return (
+		<div id='delete-confirmation'>
+			<h2>Are you sure?</h2>
+			<p>Do you really want to remove this place?</p>
+			<div id='confirmation-actions'>
+				<button onClick={onCancel} className='button-text'>
+					No
+				</button>
+				<button onClick={onConfirm} className='button'>
+					Yes
+				</button>
+			</div>
+			<progress value={remainingTime} max={TIMER} />
+		</div>
+	);
+}
