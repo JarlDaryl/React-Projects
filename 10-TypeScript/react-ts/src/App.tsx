b085f8b9933b278998c0e4035e61bcb9efe40c11
Luis <luisarquescalero@gmail.com>
2024-09-11 16:05:33 +0200

    Add NewTodo component and implement addTodoHandler

--- 10-TypeScript/react-ts/src/App.tsx
+++ 10-TypeScript/react-ts/src/App.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
-
 import Todos from './components/Todos';
 import Todo from './models/todo';
+import NewTodo from './components/NewTodo';
+
+import { useState } from 'react';
+
 
 function App() {
-  const todos = 
-    [new Todo('Learn TypeScript'),
-    new Todo ('Improve React')]
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+    const addTodoHandler = (todoText : string) => {
+      const newTodo = new Todo(todoText)
+
+      setTodos((prevTodos) => {
+        return prevTodos.concat(newTodo);
+      });
+    };
   
   return (
     <div >
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos}/>
     </div>
   );
 }
 
 export default App;
+ 
--- 10-TypeScript/react-ts/src/components/NewTodo.tsx
+++ 10-TypeScript/react-ts/src/components/NewTodo.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useRef } from 'react';
 
-const NewTodo = () => {
+import classes from './NewTodo.module.css'
+
+
+const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => {
     const todoTextInputRef = useRef<HTMLInputElement>(null);
 
     const submitHandler = (event:React.FormEvent) => {
@@ -13,10 +16,10 @@ const NewTodo = () => {
             return;
         }
 
-        
+        props.onAddTodo(enteredText);
     }   
     return (
-        <form onSubmit={submitHandler}>
+        <form onSubmit={submitHandler} className={classes.form}>
             <label htmlFor="text">Todo text</label>
             <input type="text" id='text' ref={todoTextInputRef}/>
             <button>Add Todo</button>
