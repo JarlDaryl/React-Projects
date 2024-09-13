c3c2c13175b6aaa6b2cbb7069660aebcba509ff8
Luis <luisarquescalero@gmail.com>
2024-09-13 11:05:51 +0200

    Setting up Quiz App and first steps

---
+++ 11-QuizApp/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logoImg from '../assets/quiz-logo.png'
+
+export default function Header() {
+  return (
+    <header>
+        <img src={logoImg} alt="Quiz Logo" />
+        <h1>ReactQuiz</h1>
+    </header>
+  )
+}
---
+++ 11-QuizApp/src/components/Quiz.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+import QUESTIONS from '../questions'
+
+export default function Quiz() {
+    const [userAnswers, setUserAnswers] = useState([]);
+
+    const activeQuestionIndex = userAnswers.length;
+
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers();
+
+    }
+
+  return (
+    <div id="question">
+            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+            <ul id="answers">
+                {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    <li key={answer} className='answer'>
+                        <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                    </li>
+                ))}
+            </ul>
+    </div>
+  )
+}
